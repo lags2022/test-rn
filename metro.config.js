15d682b3005651d5a0890c6c45f8739e0772506a
cesaran9 <melcabo954@gmail.com>
2025-02-17 13:41:42 -0500

    finish natiwind and restructure files directories

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require('expo/metro-config')
+const { withNativeWind } = require('nativewind/metro')
 
-const config = getDefaultConfig(__dirname);
+// Obtener la configuración predeterminada de Expo
+const config = getDefaultConfig(__dirname)
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+// Aplicar NativeWind con el archivo global.css
+module.exports = withNativeWind(config, { input: './global.css' })
